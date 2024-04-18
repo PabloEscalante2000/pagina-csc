39b6f19334ef7bd4c53aef869f276523cf0365f2
pablo escalante <56638479+PabloEscalante2000@users.noreply.github.com>
2024-04-17 20:41:55 -0500

    adicion de nuevos colaboradores

--- src/data/DataColaborator.js
+++ src/data/DataColaborator.js
@@ -2,6 +2,9 @@ import cesar from "../assets/César 2.jpg"
 import mateo from "../assets/Mateo 1.jpg"
 import mayte from "../assets/Mayte 1.jpg"
 import melissa from "../assets/Melissa 2.jpg"
+import miryam from "../assets/miryam.png"
+import nijme from "../assets/nijme-p.png"
+import jeymer from "../assets/jeymer.png"
 
 export const arrayColaborator = [
     {
@@ -11,8 +14,8 @@ export const arrayColaborator = [
         img_url:"../assets/César 2.jpg",
         name_class:"cesar",
         description:[
-            "Director de CSC Consultora Salud Corporativa",
-            "Psicólogo clínico con maestría en neuropsicología"
+            "Director de CSC Consultora Salud Corporativa.",
+            "Psicólogo clínico con maestría en neuropsicología."
         ]
     },
     {
@@ -23,7 +26,7 @@ export const arrayColaborator = [
         name_class:"mateo",
         description:[
             "Bachiller en Administración por la Universidad de Navarra con especialidad en Recursos Humanos.",
-            "Formación en psicoanálisis (facilitador en psicoterapia)"
+            "Formación en psicoanálisis (facilitador en psicoterapia)."
         ]
     },
     {
@@ -33,8 +36,8 @@ export const arrayColaborator = [
         img_url:"../assets/Melissa 2.jpg",
         name_class:"melissa",
         description:[
-            "Bachiller en derecho corporativo",
-            "Formación en psicoanálisis (facilitador en psicoterapia)"
+            "Bachiller en derecho corporativo.",
+            "Formación en psicoanálisis (facilitador en psicoterapia)."
         ]
     },
     {
@@ -44,8 +47,38 @@ export const arrayColaborator = [
         img_url:"../assets/Mayte 1.jpg",
         name_class:"mayte",
         description:[
-            "Psicóloga organizacional con especialidad en psicología empresarial",
-            "Especialización como analista de selección y evaluación de personal"
+            "Psicóloga organizacional con especialidad en psicología empresarial.",
+            "Especialización como analista de selección y evaluación de personal."
+        ]
+    },
+    {
+        name:"Jeymer Iriate Obregón",
+        cpp:null,
+        img:jeymer,
+        img_url:"../assets/jeymer.png",
+        name_class:"jeymer",
+        description:[
+            "Psicólogo organizacional especialista en selección, capacitación y desarrollo del talento humano con más de 12 años en ejercicio profesional."
+        ]
+    },
+    {
+        name:"Miryam Urrunaga Ramírez",
+        cpp:null,
+        img:miryam,
+        img_url:"../assets/miryam.png",
+        name_class:"miryam",
+        description:[
+            "Psicóloga organizacional en clima y cultura, selección y evaluación de personal, con 4 años en ejercicio profesional."
+        ]
+    },
+    {
+        name:"Nijme Morcos Gonzales",
+        cpp:null,
+        img:nijme,
+        img_url:"../assets/nijme-p.png",
+        name_class:"nijme",
+        description:[
+            "Consejera psicóloga educativa empresarial y clínico organizacional con más de 20 años en ejercicio profesional."
         ]
     }
 ]
